12b3d93d3ca67224c54ee860d90f6dcbe6115cca
yvg <yvgpost@gmail.com>
2025-07-10 12:40:26 +0200

    JS improvements

--- app.js
+++ app.js
@@ -1,99 +1,116 @@
-let cleanCell = document.getElementById("productGridClean");
+let gridClean = document.getElementById("gridClean");
 
 let shopItemsData = [   
     {
         id:'1',
-        name:"WASHCLEAN",
+        productName:"WASHCLEAN",
         shortDescription:"Silně alkalický, nepěnivý čistící a odmašťující přípravek",
-        img: "img/canister.jpg"
+        img: "img/canister.jpg",
+        link: "washclean.html"
     },
     {
         id:'2',
-        name:"FLOOR WASH",
+        productName:"FLOOR WASH",
         shortDescription:"Silně alkalický, nepěnivý čistící a odmašťující přípravek",
-        img: "img/canister.jpg" 
+        img: "img/canister.jpg",
+        link: ""
     },  
     {
         id:'3',
-        name:"GASTRO CLEAN",
+        productName:"GASTRO CLEAN",
         shortDescription:"Silně alkalický, nepěnivý čistící a odmašťující přípravek",
-        img: "img/canister.jpg" 
+        img: "img/canister.jpg",
+        link: ""
     },
     {
         id:'4',
-        name:"UNIPROF",
+        productName:"UNIPROF",
         shortDescription:"Koncentrovaný čistící přípravek na okna, voděodolné povrchy, sanitární zařízení",
-        img: "img/canister.jpg" 
+        img: "img/canister.jpg",
+        link: ""
     },
     {
         id:'5',
-        name:"RAMBO WC",
+        productName:"RAMBO WC",
         shortDescription:"Přípravek (koncentrát) na odstranění rzi a vodního kamene",
-        img: "img/canister.jpg" 
+        img: "img/canister.jpg",
+        link: ""
     },
     {
         id:'6',
-        name:"RAMBO milk",
+        productName:"RAMBO milk",
         shortDescription:"Vysoce účinný prostředek k mytí silně znečištěných rukou zejména od šmíru, oleje, ale i dalšího znečištění",
-        img: "img/canister.jpg" 
+        img: "img/canister.jpg",
+        link: ""
     },
     {
         id:'7',
-        name:"RAMBO sand",
+        productName:"RAMBO sand",
         shortDescription:"Vysoce účinný přípravek pro čištění silně znečištěných povrchů různých materiálů",
-        img: "img/canister.jpg" 
+        img: "img/canister.jpg",
+        link: ""
     },
     {
         id:'8',
-        name:"REGIA",
+        productName:"REGIA",
         shortDescription:"Antibakteriální tekuté mýdlo na ruce",
-        img: "img/canister.jpg" 
+        img: "img/canister.jpg",
+        link: ""
     },
     {
         id:'9',
-        name:"LUXSOAP",
+        productName:"LUXSOAP",
         shortDescription:"Tekuté mýdlo",
-        img: "img/canister.jpg" 
+        img: "img/canister.jpg",
+        link: ""
     },
     {
         id:'10',
-        name:"SPRING",
+        productName:"SPRING",
         shortDescription:"Mycí přípravek pro ruční mytí nádobí s vůní limetky",
-        img: "img/canister.jpg" 
+        img: "img/canister.jpg",
+        link: ""
     },
     {
         id:'11',
-        name:"RUDON",
+        productName:"RUDON",
         shortDescription:"Silně alkalický pěnivý čistící přípravek",
-        img: "img/canister.jpg" 
+        img: "img/canister.jpg",
+        link: ""
     },
     {
         id:'12',
-        name:"SOFT FLOOR",
+        productName:"SOFT FLOOR",
         shortDescription:"Alkalický pěnivý čistící přípravek",
-        img: "img/canister.jpg" 
+        img: "img/canister.jpg",
+        link: ""
     },
     {
         id:'13',
-        name:"NIOSEPTOR",
+        productName:"NIOSEPTOR",
         shortDescription:"Odpuzovač vody, ochranná leštěnka nerezu",
-        img: "img/canister.jpg" 
+        img: "img/canister.jpg",
+        link: ""
     }]
 
 let generateCleanCell = () => {
-    return (cleanCell.innerHTML = shopItemsData.map((x)=>{
+    return (gridClean.innerHTML = shopItemsData.map((x)=>{
+        let = {id, productName, shortDescription, img, link} = x;
         return `
-        <div class="product-cell" id="productCellClean">
-          <div class="product-cell-left">
-            <h2>${x.name}</h2>
-            <p>${x.shortDescription}</p>
-          </div>
-          <div class="product-cell-right">
-              <img src="${x.img}">
-          </div>
-        </div>
+            <div id="product-id-${id}" onclick="window.open('${link}','_top' ); return false;">
+                <div class="product-cell" id="productCellClean">
+                    <div class="product-cell-left">
+                        <h2>${productName}</h2>
+                        <p>${shortDescription}</p>
+                    </div>
+                    <div class="product-cell-right">
+                        <img src="${img}">
+                    </div>
+                </div>
+            </div>
     `;
     }).join(""));
 };
+
 generateCleanCell();
 
